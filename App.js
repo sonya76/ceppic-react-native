07fca1eab800c6573b2e087b7bf2aa3370eb96f5
Sonia Zaïna CHALAL <soniachalalceppic@gmail.com>
2022-08-18 12:44:34 +0200

    création du style de l'app

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>CEPPIC REACT-ACTIVE</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput defaultValue="Nouveau lien..." style={styles.inputLien} />
+        <Button title="Ajouter un lien" />
+      </View>
+      <View style={styles.lienContainer}>
+        <Text> Liste des liens ...</Text>
+      </View>
     </View>
   );
 }
@@ -16,5 +20,22 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  StyleTexte: {
+    color: "red",
+  },
+  inputLien: {
+    flex: 1,
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 8,
+  },
+  lienContainer: {
+    flex: 3,
+  },
+  inputContainer: {
+    flex:1,
+    flexDirection: "row",
+    alignItems:"center"
   },
 });
